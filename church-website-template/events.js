62fb7e18504990f4f11080db3d51a2d309b31296
vbbchurchlehman <vbbchurchlehman@gmail.com>
2026-06-28 10:44:37 -0400

    add event image

--- church-website-template/events.js
+++ church-website-template/events.js
@@ -6,9 +6,23 @@ async function loadPublicEvents() {
 
   publicEventsList.innerHTML = "";
 
+  if (!events.length) {
+    publicEventsList.innerHTML = `
+      <div class="event-item">
+        <span class="event-date">Soon</span>
+        <div>
+          <h3>No Events Posted Yet</h3>
+          <p>Please check back soon.</p>
+        </div>
+      </div>
+    `;
+    return;
+  }
+
   events.forEach(event => {
     publicEventsList.innerHTML += `
       <div class="event-item">
+        ${event.image_url ? `<img class="event-image" src="${event.image_url}" alt="${event.title}">` : ""}
         <span class="event-date">${event.event_date}</span>
         <div>
           <h3>${event.title}</h3>
